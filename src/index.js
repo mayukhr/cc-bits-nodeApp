8f0ef08b3cc86e5af949e8dd5bc248a21fb519e0
mayukhr <roy.mayukh@yahoo.in>
2021-04-18 16:21:43 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bp = require("body-parser");
+const path = require("path");
+const app = express();
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.use(bp.urlencoded());
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.post("/login", function (req, res) {
+  const CREDS = {
+    username: req.body.username,
+    password: req.body.password
+  };
+  if (CREDS.username === "mayukh" && CREDS.password === "12345") {
+    res.send("<h1>Welcome to Home Page.</h1>");
+    console.log(`User ${CREDS.username}...`);
+  } else {
+    res.render("error");
+  }
+});
+
+app.listen(port, function () {
+  console.log(`Listening to port ${port}...`);
+});
