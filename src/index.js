34eedd3ac78eb2a47fd0b1c184402e0573ef6eb1
Mayukh Roy <roy.mayukh@yahoo.in>
2021-04-18 16:52:35 +0530

    port updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bp = require("body-parser");
 const path = require("path");
 const app = express();
-const port = 8080;
+const port = process.env.port || 8080;
 
 app.set("view engine", "ejs");
 app.use(bp.urlencoded());
